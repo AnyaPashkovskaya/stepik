e3d4800bfb27645dde8000ff5e3ea4816ebb8b7d
AnyaPashkovskaya <mayamis99@mail.ru>
2021-01-11 16:19:16 +0300

    add test for login_pag:test_is_registration

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -6,3 +7,13 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.should_be_login_link()         # выполняем метод страницы — переходим на страницу логина
+
+
+def test_is_registration(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.should_be_login_link()
+    page.go_to_login_page() 
+    page = LoginPage(browser, browser.current_url)
+    page.should_be_login_page()
