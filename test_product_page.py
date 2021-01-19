528c211c162f5c600eb102594d2fdce90f161c75
AnyaPashkovskaya <mayamis99@mail.ru>
2021-01-19 15:32:40 +0300

    add parametrize links

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,15 @@
 from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
-def test_guest_can_add_product_to_basket(browser):
-	link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019'
+import pytest
+
+ 
+
+
+@pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
+                                  pytest.param(7, marks=pytest.mark.xfail),
+                                  8, 9])
+def test_guest_can_add_product_to_basket(browser, link):
+	link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
 	page = ProductPage(browser, link)
 	page.open()
 	page.should_be_message_about_add_new_product()
