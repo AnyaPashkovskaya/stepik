dc39d22c8db2986a1832bcf437d54b8f42996d76
AnyaPashkovskaya <mayamis99@mail.ru>
2021-01-22 18:16:39 +0300

    add should_not_be_success_message and should_dissepeard_message_after_adding_basket

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,6 +7,19 @@ class ProductPage(BasePage):
 		button_basket.click()
 		self.solve_quiz_and_get_code()
 
+	def should_not_be_success_message(self):#элемент не появляется на странице в течение заданного времени
+		assert self.is_not_element_present(*ProductPageLocators.MESSAGE_PRICE_PRODUCT), \
+       "Success message about price is presented, but should not be"
+		assert self.is_not_element_present(*ProductPageLocators.TOTAL_MESSAGE), \
+		"Success message about product is presented, but should not be"
+
+	def should_dissepeard_message_after_adding_basket(self):
+		assert self.is_disappeared(*ProductPageLocators.MESSAGE_PRICE_PRODUCT), \
+       "Success message about price product after adding price is not dissapeard"
+		assert self.is_disappeared(*ProductPageLocators.TOTAL_MESSAGE), \
+		"Success message about product after adding price is not dissapeard"
+
+
 	def should_be_see_price_product_after_add_to_basket(self):
 		price_product = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT).text
 		message_price_product = self.browser.find_element(*ProductPageLocators.MESSAGE_PRICE_PRODUCT).text
