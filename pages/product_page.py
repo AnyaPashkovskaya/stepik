8c84074ca876cbb5053204fd8b00e43911b9a899
AnyaPashkovskaya <mayamis99@mail.ru>
2021-01-25 13:01:57 +0300

    add func should_see_button_basket_view and go_to_basket_view

--- pages/product_page.py
+++ pages/product_page.py
@@ -30,6 +30,16 @@ class ProductPage(BasePage):
 		message_name_product = self.browser.find_elements(*ProductPageLocators.TOTAL_MESSAGE)[0].text
 		assert name_product == message_name_product, 'name product not equals message about name of product'
 
+	def should_see_button_basket_view(self):
+		assert self.is_element_present(*ProductPageLocators.BUTTON_BASKET_VIEW), \
+		"can't see button_basket_view"
+
+	def go_to_basket_view(self):
+		button_basket_view = self.browser.find_element(*ProductPageLocators.BUTTON_BASKET_VIEW)
+		button_basket_view.click()
+
+
+
 	def should_be_message_about_add_new_product(self):
 		self.add_product_to_basket()
 		self.should_be_see_name_product_after_add_to_basket()
