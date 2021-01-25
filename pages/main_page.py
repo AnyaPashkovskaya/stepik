187de49f4457bd62f42e5f120a4196bb4705a647
AnyaPashkovskaya <mayamis99@mail.ru>
2021-01-25 13:00:38 +0300

    add func go_to_basket and should_be_basket

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,3 +3,11 @@ from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
 class MainPage(BasePage):
 	pass
+
+	def go_to_basket(self):
+		button = self.browser.find_element(*MainPageLocators.BUTTON_BASKET_VIEW)
+		button.click()
+
+	def should_be_basket(self):
+		assert self.is_element_present(*MainPageLocators.BUTTON_BASKET_VIEW), "Button basket is not presented"
+
