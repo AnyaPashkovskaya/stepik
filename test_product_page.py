23fc5682bdce18faf6d14b6675953fdd1e0ca767
AnyaPashkovskaya <mayamis99@mail.ru>
2021-01-29 15:42:14 +0300

    create class TestUserAddToBasketFromProductPage

--- test_product_page.py
+++ test_product_page.py
@@ -1,27 +1,37 @@
 from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
 import pytest
+import time
 
  
+@pytest.mark.user
+class TestUserAddToBasketFromProductPage:
+
+	@pytest.fixture(scope="function", autouse=True)
+	def setup(self, browser):
+		link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+		page = LoginPage(browser, link)
+		page.open()
+		email = str(time.time()) + "@fakemail.org"
+		password = str(time.time())
+		page.register_new_user(email, password)
+		page.should_be_authorized_user()
+  
+	@pytest.mark.xfail
+	def test_user_cant_see_success_message_after_adding_product_to_basket(browser):
+		link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+		page = ProductPage(browser, link)
+		page.open()
+		page.should_be_message_about_add_new_product()
 
 
-# @pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
-#                                   pytest.param(7, marks=pytest.mark.xfail),
-#                                   8, 9])
-@pytest.mark.xfail
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-	link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-	page = ProductPage(browser, link)
-	page.open()
-	page.should_be_message_about_add_new_product()
-
-
-def test_guest_cant_see_success_message(browser):
-	link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
-	page = ProductPage(browser, link)
-	page.open()
-	page.should_not_be_success_message()
+	def test_user_cant_see_success_message(browser):
+		link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+		page = ProductPage(browser, link)
+		page.open()
+		page.should_not_be_success_message()
 
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
